bc6c69083d18f631cb09d94aa44008ea738b3fd3
sergey-leveyev <61402014+sergey-leveyev@users.noreply.github.com>
2022-06-11 16:42:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   // env: {
   //   DB_LOCAL_URI: "mongodb://localhost:27017/portfolio",
   // },
-  env: {
-    DB_LOCAL_URI:
-      "mongodb+srv://root:Sl0546413505@portfolio.ggsg1z3.mongodb.net/?retryWrites=true&w=majority",
-  },
 };
